440d79f1d43c31346b818cc291ec3d91db7a0981
Lilyach <106878819+Lilyach@users.noreply.github.com>
2022-06-11 00:44:32 +0500

    Delete Homework1.java

--- src/Homework1.java
+++
@@ -1,7 +0,0 @@
-public class Homework1 {
-    public static void main(String[] args) {
-        int number =Integer.parseInt("234");
-        int lengthText = "some_text".length();
-        System.out.println(number+lengthText);
-    }
-}
