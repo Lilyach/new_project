389e2095bd48dff474efad329e9d407d45bb3ba0
Lilyach <lilichf85@gmail.com>
2022-06-10 22:44:48 +0500

    task 1

---
+++ src/Homework1.java
@@ -0,0 +1,7 @@
+public class Homework1 {
+    public static void main(String[] args) {
+        int number =Integer.parseInt("234");
+        int lengthText = "some_text".length();
+        System.out.println(number+lengthText);
+    }
+}
